7d46d35cc7fc8e4e2bad65a0aeb6f677d98a0bf5
Ralph Gonzaga <gonzagaralphdainiell@gmail.com>
2026-08-22 17:24:41 +0800

    Day01 main

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() =>
+    runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Welcome Daniel',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
