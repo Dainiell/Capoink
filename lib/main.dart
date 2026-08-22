edf41a5420806bad3c295198833257b238ff0286
Ralph Gonzaga <gonzagaralphdainiell@gmail.com>
2026-08-22 19:11:19 +0800

    DAY 01 Main

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       home: const Scaffold(
         body: Center(
           child: Text(
-            'Welcome Daniel',
+            'Welcome To Capoink',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
