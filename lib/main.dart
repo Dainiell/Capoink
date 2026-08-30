ede44a269b88d3d5d01f99cbf641d13b6bcccb3e
Ralph Gonzaga <gonzagaralphdainiell@gmail.com>
2026-08-30 19:09:48 +0800

    loading animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
+import 'screens/loading_screen.dart';
+
 void main() =>
     runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
 
@@ -14,14 +16,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Welcome To Capoink!',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
+      home: const LoadingScreen(),
     );
   }
 }
