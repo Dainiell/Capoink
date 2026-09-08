1c15f81c763ee8e38ef5335be2259abe045dcf38
Ralph Gonzaga <gonzagaralphdainiell@gmail.com>
2026-09-08 22:37:51 +0800

    Welcome Screen Done

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,41 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xFFDE8787),
+        backgroundColor: Color(0xFFFFFFFF),
         body: Align(
-          alignment: Alignment(0.0, -0.7),
+          alignment: Alignment(-0.0, -0.0),
           child:
-              Text(
-                    "Welcome to Capoink",
-                    style: GoogleFonts.inter(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w800,
-                      color: Color(0xFFFFFFFF),
-                    ),
+              Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "CAP",
+                        style: GoogleFonts.inter(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xFF000000),
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/welcome_icon.png',
+                        height: 65,
+                      ),
+                      Text(
+                        "INK",
+                        style: GoogleFonts.inter(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xFF000000),
+                        ),
+                      ),
+                    ],
                   )
                   .animate()
                   .fadeIn(duration: 800.ms)
                   .blurXY(begin: 8, end: 0, duration: 800.ms),
-        ), // Align
-      ), // Scaffold
-    ), // MaterialApp
+        ),
+      ),
+    ),
   );
 }
